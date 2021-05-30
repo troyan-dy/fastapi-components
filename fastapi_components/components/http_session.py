c11433bb5fec4b386de18ef33c1df217d8c61685
troyan-dy <troyan-dy@sber-zvuk.com>
2021-05-30 11:19:31 +0300

    fix AIOHTTPSessionComponent.startup

--- fastapi_components/components/http_session.py
+++ fastapi_components/components/http_session.py
@@ -1,15 +1,17 @@
 from typing import Optional
 
 import aiohttp
+
 from fastapi_components.components.base import BaseComponent
 from fastapi_components.state import State
 
 
 class AIOHTTPSessionComponent(BaseComponent):
-    session: Optional[aiohttp.ClientSession]
+    session: Optional[aiohttp.ClientSession] = None
 
     async def startup(self, state: State) -> State:
-        state.session = aiohttp.ClientSession()
+        self.session = aiohttp.ClientSession()
+        state.session = self.session
         return state
 
     async def shutdown(self) -> None:
